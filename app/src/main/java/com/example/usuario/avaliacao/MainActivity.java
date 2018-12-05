21aa28fa2ff7cb567a2da9ed948af7d04ceab209
emanuelrufino63 <31510758+emanuelrufino63@users.noreply.github.com>
2018-12-05 17:34:33 -0300

    Layout_second

--- app/src/main/java/com/example/usuario/avaliacao/MainActivity.java
+++ app/src/main/java/com/example/usuario/avaliacao/MainActivity.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -30,8 +32,13 @@ public class MainActivity extends Activity {
     private EditText editTextPref;
     private Button btnSalvar;
     private Button btnRecuperar;
+    private Button btnTirarFoto;
     private SharedPreferences prefs;
     private String prefName = "ESTRANGEIRO";
+    final int IMAGE_REQUEST = 1;
+    final int TIRAR_FOTO = 2;
+    private TextView txtArq;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +61,16 @@ public class MainActivity extends Activity {
 
         // INÍCIO TÓPICO 11
         Button btnInserir = (Button) findViewById(R.id.btnInserir);
+
         btnInserir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent InserirContato = new Intent(Intent.ACTION_INSERT);
                 InserirContato.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                InserirContato.putExtra(ContactsContract.Intents.Insert.PHONETIC_NAME,ContactsContract.CommonDataKinds.Nickname.TYPE_DEFAULT);
+                InserirContato.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE,ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
+                InserirContato.putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE,ContactsContract.CommonDataKinds.Email.TYPE_WORK);
+
 
                 if (InserirContato.resolveActivity(getPackageManager()) != null){
                     startActivity(InserirContato);
@@ -84,32 +96,85 @@ public class MainActivity extends Activity {
         // INÍCIO TÓPICO 13
         // A ORIENTAÇÃO É QUE NO ARMAZENAMENTO EXTERNO É UM LOCAL ONDE QUALQUER UM PODE ACESSAR,
         //  PORTANTO DEVE-SE TER CUIDADO COM OS DADOS, JA QUE NO INTERNO, SÓ PODE SER ACESSO NO PRÓPRIO APP.
+
+        txtArq = findViewById(R.id.txtArq);
+        Button btnCarregarArquivo = (Button) findViewById(R.id.btnCarregarArquivo);
+        btnCarregarArquivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "Selecione uma imagem)"), IMAGE_REQUEST);
+            }
+        });
         // FIM TÓPICO 13
 
         // INÍCIO TÓPICO 15
         editTextPref = (EditText) findViewById(R.id.editTextPref);
         btnSalvar = (Button) findViewById(R.id.btnSalvar);
 
-        final SharedPreferences prefs = getSharedPreferences(prefName,Context.MODE_PRIVATE);
+        //final SharedPreferences prefs = getSharedPreferences(prefName,Context.MODE_PRIVATE);
 
         //editTextPref.setText(prefs.getString("ESTRANGEIRO",null));
 
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(editTextPref.getText() != null && !editTextPref.getText().toString().isEmpty()) {
+                    SharedPreferences prefs = getSharedPreferences(prefName,Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
 
+                    editor.putString("ESTRANGEIRO", editTextPref.getText().toString());
 
-                SharedPreferences.Editor editor = prefs.edit();
+                    editor.commit();
 
-                editor.putString("ESTRANGEIRO",editTextPref.getText().toString());
-                editor.commit();
+                    Toast.makeText(getBaseContext(), "Dados Salvos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getBaseContext(), "Informe um texto!!", Toast.LENGTH_SHORT).show();
+                }
 
-                Toast.makeText(getBaseContext(),"Dados Salvos",Toast.LENGTH_SHORT).show();
+                //SharedPreferences.Editor editor = prefs.edit();
+
+                //editor.putString("ESTRANGEIRO",editTextPref.getText().toString());
+                //editor.commit();
+
+                //Toast.makeText(getBaseContext(),"Dados Salvos",Toast.LENGTH_SHORT).show();
             }
         });
         // FIM TÓPICO 15
 
+
+        btnTirarFoto = (Button) findViewById(R.id.btnTirarFoto);
+
+        btnTirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tirarFoto();
+            }
+        });
      }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), uri);
+                txtArq.setCompoundDrawablesWithIntrinsicBounds(null, new BitmapDrawable(getResources(), bm), null, null);
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this,"Erro: "+e.getMessage().toString(),Toast.LENGTH_LONG);
+            }
+        }
+    }
+
+    private void tirarFoto() {
+        Intent takePictureIntent = new
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, TIRAR_FOTO);
+        }
+    }
 
 }
