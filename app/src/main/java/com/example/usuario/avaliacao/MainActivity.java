429e59f0e1004f06e536ef0d9dd6c4358220bf6f
emanuelrufino63 <31510758+emanuelrufino63@users.noreply.github.com>
2018-12-05 17:58:31 -0300

    Layout_second

--- app/src/main/java/com/example/usuario/avaliacao/MainActivity.java
+++ app/src/main/java/com/example/usuario/avaliacao/MainActivity.java
@@ -4,17 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
-import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,16 +17,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
+
 
 public class MainActivity extends Activity {
 
     private EditText editTextPref;
     private Button btnSalvar;
-    private Button btnRecuperar;
     private Button btnTirarFoto;
     private SharedPreferences prefs;
     private String prefName = "ESTRANGEIRO";
@@ -47,7 +39,6 @@ public class MainActivity extends Activity {
 
         // INÍCIO TÓPICO 10
         final EditText editText = (EditText) findViewById(R.id.editText);
-        //ImageView imagem = (ImageView) findViewById(R.id.ivImagem);
 
         Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
         btnEnviar.setOnClickListener(new View.OnClickListener() {
@@ -114,10 +105,6 @@ public class MainActivity extends Activity {
         editTextPref = (EditText) findViewById(R.id.editTextPref);
         btnSalvar = (Button) findViewById(R.id.btnSalvar);
 
-        //final SharedPreferences prefs = getSharedPreferences(prefName,Context.MODE_PRIVATE);
-
-        //editTextPref.setText(prefs.getString("ESTRANGEIRO",null));
-
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -134,12 +121,6 @@ public class MainActivity extends Activity {
                     Toast.makeText(getBaseContext(), "Informe um texto!!", Toast.LENGTH_SHORT).show();
                 }
 
-                //SharedPreferences.Editor editor = prefs.edit();
-
-                //editor.putString("ESTRANGEIRO",editTextPref.getText().toString());
-                //editor.commit();
-
-                //Toast.makeText(getBaseContext(),"Dados Salvos",Toast.LENGTH_SHORT).show();
             }
         });
         // FIM TÓPICO 15
